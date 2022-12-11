c3400ae519dea17f094674dccf58668f182f74f3
Grisha <micronesia@mail.ru>
2022-12-11 18:18:39 +0300

    Массив

--- FinalTask/FinalTaskHomework.cs
+++ FinalTask/FinalTaskHomework.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[5] {"Football", "13", "Hello", "World", "WTF"};
+string[] array2 = new string[array1.Length];
+
