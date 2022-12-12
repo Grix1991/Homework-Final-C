f8536745ed02b4407ed11986082d90ab0792d934
Grisha <micronesia@mail.ru>
2022-12-12 20:31:40 +0300

    Тело

--- FinalTask/FinalTaskHomework.cs
+++ FinalTask/FinalTaskHomework.cs
@@ -1,3 +1,15 @@
-﻿string[] array1 = new string[5] {"Football", "13", "Hello", "World", "WTF"};
+﻿string[] array1 = new string[5] {"Cup", "13", "Hello", "World", "WTF"};
 string[] array2 = new string[array1.Length];
 
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
